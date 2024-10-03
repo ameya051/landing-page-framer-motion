d923099ac75fe9b4c65fedb07336244d2f73e3bd
Ameya Shrivastava <ameyshri051@gmail.com>
2024-10-03 22:49:22 +0530

    fix build, create logo ticker

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -4,8 +4,10 @@ import Button from "@/components/Button";
 export const Header = () => {
   return (
     <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
+      <div className="absolute inset-0 -z-10 backdrop-blur md:backdrop-blur-none md:hidden" />
       <div className="container">
-        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto backdrop-blur">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto relative">
+          <div className="absolute inset-0 hidden md:block md:backdrop-blur -z-10" />
           <div>
             <div className="border border-white/15   h-10 w-10 rounded-lg inline-flex items-center justify-center">
               <LogoIcon className="w-8 h-8" />
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,23 @@
+import acmeLogo from "@/assets/logo-acme.png"
+import apexLogo from "@/assets/logo-apex.png"
+import celestialLogo from "@/assets/logo-celestial.png"
+import quantumLogo from "@/assets/logo-quantum.png"
+import pulseLogo from "@/assets/logo-pulse.png"
+import echoLogo from "@/assets/logo-echo.png"
+
 export const LogoTicker = () => {
-  return <section>LogoTicker</section>;
+  const logos = [acmeLogo, apexLogo, celestialLogo, quantumLogo, pulseLogo, echoLogo]
+  return <div className="container py-20 md:py-24">
+    <div className="flex items-center gap-5">
+      <div className="flex-1 md:flex-none"><h1>Trusted by top innovative teams</h1></div>
+      <div className="flex-1 overflow-hidden">
+        <div className="flex flex-none gap-14 [mask-image:linear-gradient(to_right,transparent,black,black_20%,black_80%,transparent)]">
+          {
+            logos.map((logo) => <img src={logo.src} key={logo.src} className="h-6 w-auto" />)
+          }
+        </div>
+      </div>
+
+    </div>
+  </div>;
 };
