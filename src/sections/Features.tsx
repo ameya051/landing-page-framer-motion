a057c3009502861415e224fcc1ccadb11ae561bd
Ameya Shrivastava <ameyshri051@gmail.com>
2024-10-06 21:36:49 +0530

    created product showcase section

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,9 @@
+"use client"
+
+import { DotLottiePlayer } from "@dotlottie/react-player"
+import productImg from "@/assets/product-image.png"
+import Image from "next/image";
+
 const tabs = [
   {
     icon: "/assets/lottie/vroom.lottie",
@@ -26,5 +32,28 @@ const tabs = [
 ];
 
 export const Features = () => {
-  return <section>Features</section>;
+  return <section className="py-20 md:py-24">
+    <div className="container">
+      <h2 className="text-5xl md:text-6xl font-medium text-center tracking-tighter">Elevate your SEO efforts</h2>
+      <p className="text-white/70 text-lg md:text-xl max-w-2xl mx-auto tracking-tight text-center mt-5">
+        From small startups to large enterprises, our AI-driven tool has revolutionized the way businesses approach SEO.
+      </p>
+      <div className="mt-10 flex flex-col lg:flex-row gap-3">
+        {
+          tabs.map((tab) => <div key={tab.title} className="border border-white/15 flex lg:flex-1 items-center p-2.5 rounded-xl gap-2.5">
+            <div className="size-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
+              <DotLottiePlayer autoplay src={tab.icon} className="size-5" />
+            </div>
+            <div className="font-medium">{tab.title}</div>
+            {tab.isNew && <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">new</div>}
+          </div>)
+        }
+      </div>
+      <div className="border border-white/20 p-2.5 rounded-xl mt-3">
+        <div className="aspect-video bg-cover border border-white/20 rounded-lg" style={{ backgroundImage: `url(${productImg.src})` }}>
+        </div>
+        {/*<Image src={productImg} alt="Product Image" />*/}
+      </div>
+    </div>
+  </section>;
 };
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -17,7 +17,6 @@ export const LogoTicker = () => {
           }
         </div>
       </div>
-
     </div>
   </div>;
 };
